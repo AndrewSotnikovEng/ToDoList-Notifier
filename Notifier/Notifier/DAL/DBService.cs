2e5ed9839a94dc298227bec0d69410e82732f4ec
AndrewSotnikovEng <andrew.sotnikov.eng@gmail.com>
2022-02-08 12:45:48 +0200

    Implemented first DAO

---
+++ Notifier/Notifier/DAL/DBService.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.OleDb;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Notifier.DataLayer
+{
+    class DBService
+    {
+
+        const int CURRENT_TASK_SPACE_ID = 1;
+        const int BACKLOG_TASK_SPACE_ID = 2;
+
+        OleDbConnection Connection { get; set; }
+        OleDbCommand Command { get; set; }
+        string SQL { get; set; }
+        string ConnectionString { get; set; }
+
+        // Open connecton    
+
+        public DBService(string dbFile)
+        {   
+            if (!File.Exists(dbFile))
+            {
+                Console.WriteLine("Database wasn't found!");
+                return;
+            }
+            ConnectionString = $"Provider=Microsoft.ACE.OLEDB.12.0;Data Source=\"{dbFile}\"";
+            
+            Connection = new OleDbConnection(ConnectionString);
+            Connection.Open();
+        }
+
+        public void CreateNewTask(string taskName, bool isCurrent )
+        {
+            SQL = $"INSERT INTO Tasks (Name) VALUES ('{taskName}')";
+            Command = new OleDbCommand(SQL, Connection);
+            Command.ExecuteReader();
+
+            
+
+            if (isCurrent)
+            {
+                //finding ID of recently added task
+                SQL = $"SELECT ID FROM Tasks WHERE Name LIKE '{taskName}'";
+                Command = new OleDbCommand(SQL, Connection);
+                OleDbDataReader reader = Command.ExecuteReader();
+                reader.Read();
+                string taskId = reader["ID"].ToString();
+
+
+                //adding it to context
+                SQL = $"INSERT INTO Contexts(TaskID, SpaceID, ActualDate) VALUES({taskId},{CURRENT_TASK_SPACE_ID},FORMAT(#{GetCurDate()}# , 'yyyy/mm/dd'))";
+                Command = new OleDbCommand(SQL, Connection);
+                Command.ExecuteReader();
+                reader.Read();
+            }
+        }
+
+        public List<string> GetAllTaskNamesFromCurrent(bool verbose)
+        {
+            List<String> taskNames = new List<string>();
+            SQL = "SELECT Tasks.ID, " +
+                 "Tasks.Name, " +
+                 "Contexts.ActualDate, " +
+                 "Contexts.SpaceID " +
+                 "FROM Tasks " +
+                 "INNER JOIN Contexts ON Tasks.ID = Contexts.TaskID " +
+                 "WHERE(((Contexts.SpaceID) = 1));";
+
+            // Create a command and set its connection    
+            Command = new OleDbCommand(SQL, Connection);
+
+            // Execute command    
+            OleDbDataReader reader = Command.ExecuteReader();
+
+            Console.WriteLine("------------Original data----------------");
+            while (reader.Read())
+            {
+                string taskName = String.Format("{0} : {1}", reader["Name"].ToString(), reader["ID"].ToString());
+                taskNames.Add(taskName);
+                if (verbose)
+                {
+                    Console.WriteLine(taskName);
+                }
+            }
+            return taskNames;
+        }
+
+        string GetCurDate()
+        {
+            string curDate = $"{DateTime.Now.Year.ToString("00")}/{DateTime.Now.Month.ToString("00")}/{DateTime.Now.Day.ToString("00")}";
+
+            return curDate;
+        }
+
+
+
+    }
+}
---
+++ Notifier/Notifier/MainWindow.xaml.cs
@@ -0,0 +1,142 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Data.OleDb;
+using System.Data;
+using System.Windows.Forms;
+using Notifier.DataLayer;
+
+namespace Notifier
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        DBService service;
+
+        public MainWindow()
+        {
+
+            InitializeComponent();
+            service = new DBService(@"D:\Дело\das_code\dotnet\todo-list.accdb");
+            LoadTasksFromDb();
+
+        }
+
+        void LoadTasksFromDb()
+        {
+            foreach (var item in service.GetAllTaskNamesFromCurrent(true))
+            {
+                tasksListBox.Items.Add(item);
+            }
+
+ 
+        }
+
+        private void taskListBox_DoubleClick(object sender, MouseButtonEventArgs e)
+        {
+
+        }
+
+        private void AddBtn_Click(object sender, RoutedEventArgs e)
+        {
+            var dialog = new UserInput();
+            if (dialog.ShowDialog() == true)
+            {
+
+            }
+
+            if (!string.IsNullOrEmpty(dialog.ResponseTextBox.Text))
+            {
+                string taskName = dialog.ResponseTextBox.Text;
+                if (System.Windows.MessageBox.Show("Do you want to add new record?",
+                        "Confirmation", MessageBoxButton.YesNo) == MessageBoxResult.Yes)
+                {
+
+                    if (dialog.CurrentComboBox.IsChecked == true)
+                    {
+                        service.CreateNewTask(taskName, true);                        
+                    } else
+                    {
+                        service.CreateNewTask(taskName, false);
+                    }
+                    UpdateListBox();
+                }
+                else
+                { }
+            }
+        }
+
+
+        void UpdateListBox()
+        {
+            tasksListBox.Items.Clear();
+            LoadTasksFromDb();
+            tasksListBox.UpdateLayout();
+        }
+
+        private void DoneBtn_Click(object sender, RoutedEventArgs e)
+        {
+            //string selectedItem;
+            //if (!String.IsNullOrEmpty(tasksListBox.SelectedItem.ToString()))
+            //{
+            //    selectedItem = tasksListBox.SelectedItem.ToString();
+            //}
+            //else
+            //{
+            //    return;
+            //}
+
+
+            //int taskId = Int32.Parse(selectedItem.Split(':')[0]);
+
+            //SQL = $"SELECT IsTemplate FROM Tasks WHERE ID ={taskId}";
+            //command = new OleDbCommand(SQL, connection);
+            //OleDbDataReader reader = command.ExecuteReader();
+            //reader.Read();
+            //bool isTemplate = reader["IsTemplate"].ToString() == "True" ? true : false;
+
+            //string curDate = $"{DateTime.Now.Year.ToString("00")}/{DateTime.Now.Month.ToString("00")}/{DateTime.Now.Day.ToString("00")}";
+            //if (isTemplate) //if tempalte
+            //{
+            //    SQL = $"UPDATE Tasks SET CompletionDate = FORMAT(#{curDate}# , 'yyyy/mm/dd') WHERE ID = {taskId}";
+            //    command = new OleDbCommand(SQL, connection);
+            //    reader = command.ExecuteReader();
+            //    reader.Read();
+
+            //} else //if not tempalte
+            //{                
+
+            //    SQL = $"UPDATE Tasks SET Completed = True, CompletionDate = FORMAT(#{curDate}# , 'yyyy/mm/dd') WHERE ID = {taskId}";
+            //    command = new OleDbCommand(SQL, connection);
+            //    reader = command.ExecuteReader();
+            //    reader.Read();
+
+            //}
+            //SQL = $"DELETE FROM Contexts WHERE TaskID = {taskId}";
+            //command = new OleDbCommand(SQL, connection);
+            //reader = command.ExecuteReader();
+            //reader.Read();
+
+
+            //UpdateListBox();
+        }
+
+        private void RegreshBtn_Click(object sender, RoutedEventArgs e)
+        {
+            UpdateListBox();
+        }
+    }
+}
